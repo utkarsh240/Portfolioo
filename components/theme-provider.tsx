d0b083dae41be253b9d79c306651e4efe889730a
Utkarsh <93428291+utkarsh240@users.noreply.github.com>
2025-07-17 17:14:13 +0530

    my changes

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -8,7 +8,7 @@ export function ThemeProvider({
   ...props 
 }: {
   children: React.ReactNode
-  [key: string]: any
+  [key: string]: unknown
 }) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 } 
